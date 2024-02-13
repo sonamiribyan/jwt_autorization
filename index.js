0545acc33b6a8c5b40e6f41ad0cb11cfd47db4d0
Sona Miribyan <sona.miribyan2004@gmail.com>
2024-02-13 10:49:35 +0400

    static

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static('static'))
+
 app.use('/api', router);
 app.use(errorHandler);
 async function start() {
